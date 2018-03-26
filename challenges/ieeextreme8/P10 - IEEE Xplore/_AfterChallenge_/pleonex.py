d01e8c173484a1eb87f14775e529e2c0396eb90e
Benito Palacios Sanchez <benito356@gmail.com>
2018-03-26 13:43:25 +0200

    Improve P10 solution

--- challenges/ieeextreme8/P10 - IEEE Xplore/_AfterChallenge_/pleonex.py
+++ challenges/ieeextreme8/P10 - IEEE Xplore/_AfterChallenge_/pleonex.py
@@ -1,43 +1,49 @@
 # -*- coding: utf-8 -*-
-"""Solution to problem 10 by pleonex."""
+"""Solution to problem 10."""
 
 
-def create_num(word):
-    """Create a num from a word."""
+def get_number(word):
+    """Get a word from the number."""
     num = 0
-    for char in word[::-1]:
+    for c in word[::-1]:
         num <<= 8
-        num |= ord(char)
+        num |= ord(c)
     return num
 
 
-def create_combinations(elements):
-    """Create all the possible combinations."""
-    combinations = set()
-    if len(elements) == 1:
-        return set(elements[0])
+def generate_anagrams(chars):
+    """Generate an anagram incrementing a number."""
+    anagrams = []
 
-    for i in range(len(elements)):
-        copy = elements[:]
-        del copy[i]
-        subcombinations = create_combinations(copy)
-        for comb in subcombinations:
-            combinations.add(elements[i] + comb)
-    return combinations
+    if len(chars) == 1:
+        return [chars[0]]
+
+    for i in range(len(chars)):
+        cpy = chars[:]
+        del cpy[i]
+
+        combinations = generate_anagrams(cpy)
+        for c in combinations:
+            anagrams.append(chars[i] + c)
+
+    return anagrams
 
 
 def main(line):
     """Main application logic."""
+    # Filter the input, it ends with the dot.
     line = line[:line.find(".")]
-    elements = [e for e in line]
 
-    combinations = create_combinations(elements)
-    numbers = [create_num(c) for c in combinations]
-
+    limit = get_number(line)
+    # print limit
+    chars = [c for c in line]
+    anagrams = generate_anagrams(chars)
+    # print anagrams
+    num = [get_number(w) for w in anagrams]
+    # print num
     count = 0
-    limit = create_num(line)
-    for num in numbers:
-        if num <= limit:
+    for n in num:
+        if n <= limit:
             count += 1
     print "%02d" % count
 
